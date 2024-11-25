d7fc2159fda03d98d1a0f632e57a6e7e989bcebd
Mactyws <mattiagirellini165@gmail.com>
2024-11-25 21:06:50 +0100

    PLEASE

--- domanitiinterrogo/settings.py
+++ domanitiinterrogo/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-b$#lwy9qplj5bf5ogb+5st-&w5*nwvbkbl5427f$%rd))r11dm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'http://*.domanitiinterrogo.website', 'https://*s.domanitiinterrogo.website']
+ALLOWED_HOSTS = ['*', 'http://*.domanitiinterrogo.website', 'https://*.domanitiinterrogo.website', 
+                      'http://domanitiinterrogo.website/*', 'https://domanitiinterrogo.website/*',
+                      'http://*.domanitiinterrogo.website/*', "https://*.domanitiinterrogo.website/*"]
 
 
 # Application definition
